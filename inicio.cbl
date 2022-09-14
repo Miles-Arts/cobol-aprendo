708e25a60c6f8c6815de3ddb0f37e18dc01b70f7
Milton Figueredo <82297475+Miles-Arts@users.noreply.github.com>
2022-09-13 19:49:03 -0500

    Add files via upload
    class first

---
+++ inicio.cbl
@@ -0,0 +1,26 @@
+      ******************************************************************
+      * Author: Milton Figueredo
+      * Date: 2022 / 09 / 12
+      * Purpose: Estudio
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. "INICIO".
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hola mi gente ¿qué cuentan?".
+            DISPLAY "Hola mi gente ¿qué cuentan?".
+
+       ABRIR-PROGRAMA.
+
+            DISPLAY "hOLA".
+            DISPLAY "BIEN?".
+
+       CERRAR-PROGRAMA.
+
+
+            STOP RUN.
+       END PROGRAM "INICIO".
