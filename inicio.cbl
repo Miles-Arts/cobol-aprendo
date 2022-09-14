5ecdf4922d7c5beead83b58dab59dc2502f97ecb
Milton Figueredo Miles <gratno@gmail.com>
2022-09-13 20:38:30 -0500

    Create inicio.cbl

---
+++ inicio.cbl
@@ -0,0 +1,40 @@
+      ******************************************************************
+      * Author: Milton Figueredo
+      * Date: 2022 / 09 / 12
+      * Purpose: Estudio
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. "INICIO".
+       AUTHOR.     MILTON FIGUEREDO.
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       SOURCE-COMPUTER. GRATNO.
+       OBJECT-COMPUTER. GRATNO.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       SPECIAL-NAMES.
+           DECIMAL-POINT IS COMMA.
+
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+
+
+
+
+       MAIN-PROCEDURE.
+            DISPLAY "Hola mi gente ¿qué cuentan?".
+            DISPLAY "Hola mi gente ¿qué cuentan?".
+
+       ABRIR-PROGRAMA.
+
+            DISPLAY "hOLA".
+            DISPLAY "BIEN?".
+
+       CERRAR-PROGRAMA.
+
+
+            STOP RUN.
+       END PROGRAM "INICIO".
